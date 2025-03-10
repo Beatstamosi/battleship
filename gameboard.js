7990e2a5fcc2c098f3cc1e2d10871604eb62ac8d
Beatstamosi <moritz.lued@gmail.com>
2025-03-10 18:24:02 -0500

    Add pseudocode gameboard.js

---
+++ gameboard.js
@@ -0,0 +1,41 @@
+export default class Gameboard {
+
+}
+
+
+// create board array
+    // for loop 10 times i
+        // for loop for each i add 10 times j
+            // add object to each field
+                // missed: false
+                // hit: false
+                // ship: null
+
+
+// function placeShip
+    // param: starting coordinate, vert or horizontal, length
+    // create new Ship with length
+    // from starting coord go up or down for length
+        // link new Ship to field object - ship
+
+
+// receive Attack
+    // param: coordinates
+    // access board array
+        // if field has ship
+            // call ship.hit()
+            // set field.hit = true
+        // else
+            // set field.missed = false
+
+
+// all Ships sunk
+    // array 
+    // placeShip adds new Ship to array
+    // return if all ship.sunk === true
+
+
+
+
+
+
